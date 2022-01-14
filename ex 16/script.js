095aa0b8ef188acb9cf6f178ddd98aef6bc83c92
Gabi273 <96780484+Gabi273@users.noreply.github.com>
2022-01-14 20:31:32 +0200

    Add files via upload

---
+++ ex 16/script.js
@@ -0,0 +1,7 @@
+
+const button = document.querySelector(".hamburger-1");
+const navbar = document.querySelector(".navbarul");
+
+button.addEventListener("click",() =>{
+    navbar.classList.toggle("open");
+})
